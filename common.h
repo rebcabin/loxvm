8d3144e78d2bf673397bc21f457a3f2acc36d57c
rebcabin <bc.beckman@gmail.com>
2021-08-01 07:11:11 -0700

    stub compiler

--- common.h
+++ common.h
@@ -11,6 +11,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include <stdio.h>
 
--- main.c
+++ main.c
@@ -35,10 +35,76 @@ void testVM() {
 }
 
 
-int main() {
+static InterpretResult
+interpret_source (const char * source) {
+    return INTERPRET_OK;
+}
+
+
+static void repl() {
+    static char line [1024];
+    for (;;) {
+        printf("lox> ");
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        interpret_source(line);
+    }
+}
+
+
+static char * readFile (const char * path) {
+    FILE * file = fopen(path, "rb");
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char * buffer = (char *)malloc(fileSize + 1);
+    assert (buffer != NULL);
+    size_t bytesRead = fread(buffer, sizeof (char), fileSize, file);
+    assert (bytesRead == fileSize);
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+
+void runFile (const char * path) {
+    char * source = readFile(path);
+    InterpretResult result = interpret_source(source);
+    free (source);
+
+    if (result == INTERPRET_COMPILE_ERROR)
+        exit (65);
+
+    if (result == INTERPRET_RUNTIME_ERROR)
+        exit (75);
+}
+
+
+void run_lox(int argc, const char ** argv) {
+    if (argc == 1) {
+        repl();
+    } else if (argc == 2) {
+        runFile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: lox [path]\n");
+    }
+}
+
+
+int main(int argc, const char ** argv) {
     initVM();
+    run_lox(argc, argv);
+
+
     testChunk();
     testVM();
     freeVM();
+
+
     return 0;
 }
